185a4211f4253cae1db8c2ea7ac2605c15e093d0
ale <ale@ale.com>
2021-10-11 16:53:46 -0300

    correção entrega

--- src/katas1.js
+++ src/katas1.js
@@ -1,123 +1,128 @@
+
 function oneThroughTwenty(x) {
-    let end = x
-    let count = 1
-    while(count <= x){
-     console.log(count)
-      count++
-    }
-}
+  let meuRetorno = [];
 
-(oneThroughTwenty(20))
+  for (let counter = 1; counter <= x; counter++) {
+      meuRetorno.push(counter)
+  }
+  
+  return meuRetorno;
+  }
+console.log(oneThroughTwenty(20))
 
-console.log('---------------------------------------------------------')
 function evensToTwenty(x) {
-    let end = x
+    let list = []
     let count = 1
     while(count <= x){
       if(count % 2 === 0){
-        console.log(count)
+        list.push(count)
       }
-      count ++
+      count++
     }
+    return list
   
 }
-
-evensToTwenty(20)
-console.log('---------------------------------------------------------')
+console.log(evensToTwenty(20))
 
 function oddsToTwenty(x) {
-  let end = x
-  for(let start = 1; start <= x; start++){
-    if(start % 2 === 1){
-      console.log(start)
+  let list = []
+  for(let count = 1; count <= x; count++){
+    if(count % 2 === 1){
+      list.push(count)
     }
   }
-   
+   return list
 }
 
-oddsToTwenty(20)
+console.log(oddsToTwenty(20))
 
-console.log('---------------------------------------------------------')
 
 function multiplesOfFive(x) {
+  let list = []
   for(let count = 5; count <= x; count++){
     if(count % 5 === 0){
-      console.log(count)
+       list.push(count)
     }
   }
-
+    return list
 }
 
-multiplesOfFive(100)
+console.log(multiplesOfFive(100))
 
-console.log('---------------------------------------------------------')
 
 function squareNumbers(x){
+  let list = []
   for(let count = 1; count <= x; count++){
     let value = count * count
     if(value === count * count){
-      console.log(value)
+       list.push(value)
     }     
   }
+    return list
 }  
 
-squareNumbers(10)
+console.log(squareNumbers(10))
 
-console.log('---------------------------------------------------------')
 
 function countingBackwards(x) {
+  let list = []
   let count = 20
-  let end   = x
   while(count >= x){
-    console.log(count)
+     list.push(count)
     count--
   }
+    return list
 }
 
-countingBackwards(1)
-console.log('---------------------------------------------------------')
+console.log(countingBackwards(1))
 
 function evenNumbersBackwards(x) {
+  let list = []
   for(let count = 20; count >= x; count--){
     if(count % 2 === 0){
-      console.log(count)
+      list.push(count) 
     }
   }
+    return list
 }
 
-evenNumbersBackwards(1)
-console.log('---------------------------------------------------------')
+console.log(evenNumbersBackwards(1))
 
-function oddNumbersBackwards(x) {
- let count = 20
- do{
-   if(count % 2 === 1)
-   console.log(count)
-   count--
- }while(count >=x)
+function oddNumbersBackwards(x){
+  let list = []
+  let count = 20
+  while(count >= x){
+    if(count % 2 === 1){
+      list.push(count)   
+    }
+    count--
+  }  
+    return list
 }
 
-oddNumbersBackwards(1)
-console.log('---------------------------------------------------------')
+console.log(oddNumbersBackwards(1))
 
 function multiplesOfFiveBackwards(x) {
- let count = 100
+  let list = []
+  let count = 100
   while(count >= x){
     if(count % 5 === 0){
-      console.log(count)
+      list.push(count);
     }
     count--
   }
+    return list
 }
 
-multiplesOfFiveBackwards(5)
-console.log('---------------------------------------------------------')
+console.log(multiplesOfFiveBackwards(5))
 
 function squareNumbersBackwards(x) {
-   for(let count = 10; count >= x; count--){
+  let list = [] 
+  for(let count = 10; count >= x; count--){
      let value = count * count
-     console.log(value)
+      list.push(value);
    }
+    return list
 }
 
-squareNumbersBackwards(1)
+console.log(squareNumbersBackwards(1))
