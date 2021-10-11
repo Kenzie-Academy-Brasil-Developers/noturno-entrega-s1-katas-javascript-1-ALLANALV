8730a8c4076c99b10ca11f491d1b4f8c982cab63
ale <ale@ale.com>
2021-10-10 23:24:03 -0300

    Entrega realizada

--- src/katas1.js
+++ src/katas1.js
@@ -1,94 +1,123 @@
-function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
+function oneThroughTwenty(x) {
+    let end = x
+    let count = 1
+    while(count <= x){
+     console.log(count)
+      count++
+    }
 }
 
-//call function oneThroughTwenty
+(oneThroughTwenty(20))
 
-function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
+console.log('---------------------------------------------------------')
+function evensToTwenty(x) {
+    let end = x
+    let count = 1
+    while(count <= x){
+      if(count % 2 === 0){
+        console.log(count)
+      }
+      count ++
+    }
   
 }
 
-//call function evensToTwenty
+evensToTwenty(20)
+console.log('---------------------------------------------------------')
 
-function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+function oddsToTwenty(x) {
+  let end = x
+  for(let start = 1; start <= x; start++){
+    if(start % 2 === 1){
+      console.log(start)
+    }
+  }
    
 }
 
-//call function oddsToTwenty
+oddsToTwenty(20)
 
-function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+console.log('---------------------------------------------------------')
+
+function multiplesOfFive(x) {
+  for(let count = 5; count <= x; count++){
+    if(count % 5 === 0){
+      console.log(count)
+    }
+  }
 
 }
 
-//call function multiplesOfFive
+multiplesOfFive(100)
 
-function squareNumbers() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-    
+console.log('---------------------------------------------------------')
+
+function squareNumbers(x){
+  for(let count = 1; count <= x; count++){
+    let value = count * count
+    if(value === count * count){
+      console.log(value)
+    }     
+  }
+}  
+
+squareNumbers(10)
+
+console.log('---------------------------------------------------------')
+
+function countingBackwards(x) {
+  let count = 20
+  let end   = x
+  while(count >= x){
+    console.log(count)
+    count--
+  }
 }
 
-//call function squareNumbers
+countingBackwards(1)
+console.log('---------------------------------------------------------')
 
-function countingBackwards() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+function evenNumbersBackwards(x) {
+  for(let count = 20; count >= x; count--){
+    if(count % 2 === 0){
+      console.log(count)
+    }
+  }
 }
 
-//call function countingBackwards
+evenNumbersBackwards(1)
+console.log('---------------------------------------------------------')
 
-function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+function oddNumbersBackwards(x) {
+ let count = 20
+ do{
+   if(count % 2 === 1)
+   console.log(count)
+   count--
+ }while(count >=x)
 }
 
-//call function evenNumbersBackwards
+oddNumbersBackwards(1)
+console.log('---------------------------------------------------------')
 
-function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+function multiplesOfFiveBackwards(x) {
+ let count = 100
+  while(count >= x){
+    if(count % 5 === 0){
+      console.log(count)
+    }
+    count--
+  }
 }
 
-//call function oddNumbersBackwards
+multiplesOfFiveBackwards(5)
+console.log('---------------------------------------------------------')
 
-function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+function squareNumbersBackwards(x) {
+   for(let count = 10; count >= x; count--){
+     let value = count * count
+     console.log(value)
+   }
 }
 
-//call function multiplesOfFiveBackwards
-
-function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function squareNumbersBackwards
+squareNumbersBackwards(1)
